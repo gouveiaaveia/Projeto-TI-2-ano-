72e8c9a148cb69d53a6163cd0394a73b35a4a569
gouveiaaveia <63357887+gouveiaaveia@users.noreply.github.com>
2024-10-01 17:30:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,40 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Load the Excel file
-exelFile = "CarDataset.xlsx"
-data = pd.read_excel(exelFile)
+def main():
 
-varNames = data.columns.values.tolist()
+    # Load the Excel file
+    exelFile = "CarDataset.xlsx"
+    data = pd.read_excel(exelFile)
 
-j = 0
+    varNames = data.columns.values.tolist()
+
+    j = 0
 
 
-for i in varNames:
-    if i != "MPG":
-        plt.subplot(3, 2, j+1)
-        plt.plot(data[i], data["MPG"], ".m")
-        plt.title(f"MPG vs {i}")
-        plt.xlabel(i)
-        plt.ylabel("MPG")
-        j += 1
-        
-    if j >= 6:
-        break
+    for i in varNames:
+        if i != "MPG":
+            plt.subplot(3, 2, j+1)
+            plt.plot(data[i], data["MPG"], ".m")
+            plt.title(f"MPG vs {i}")
+            plt.xlabel(i)
+            plt.ylabel("MPG")
+            j += 1
+            
+        if j >= 6:
+            break
 
-# Ajustar layout
-plt.subplots_adjust(hspace=1.4)
-plt.subplots_adjust(wspace=0.5)
+    # Ajustar layout
+    plt.subplots_adjust(hspace=1.4)
+    plt.subplots_adjust(wspace=0.5)
 
-plt.show()
-dd
+    plt.show()
+
+    data = np.uint16(data)
+
+    
+
+
+
+if __name__ == "__main__":
+    main()
